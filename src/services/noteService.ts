04857636f7a3fee22ee38a21bd8017953b32750f
Viktor Matviichuk <vmatviychuk85.com@gmail.com>
2026-07-28 11:12:23 +0300

    refactor: move FetchNotesResponse interface definition for improved organization

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import type { FetchNotesResponse, Note } from '../types/note';
+import type { Note } from '../types/note';
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
 
 const api = axios.create({
   baseURL: 'https://notehub-public.goit.study/api',
